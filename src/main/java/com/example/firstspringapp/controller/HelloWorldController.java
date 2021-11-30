a3899acaa8eb8f2d85880e1fe19ffe1509bcb887
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-11-30 17:54:33 +0530

    Added:welcome to the spring app

--- src/main/java/com/example/firstspringapp/controller/HelloWorldController.java
+++ src/main/java/com/example/firstspringapp/controller/HelloWorldController.java
@@ -1,16 +1,31 @@
 package com.example.firstspringapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/welcome")
 public class HelloWorldController {
+
     @GetMapping("hello")
-        public String getHelloMessage() {
+    public String getHelloMessage() {
         return "hello from bridgelabs";
-
-
     }
+//
+//    @PostMapping(value = "/test")
+//    public String test2(@RequestHeader(name = "token") String usertoken) {
+//        return usertoken;
+//    }
+//
+//    @GetMapping("/param/{name}")
+//    public String sayHelloParam(@PathVariable String name) {
+//        return "hello" + ".." + name;
+//    }
+//
+//    @PostMapping("/post")
+//    public String sayHello(@RequestBody List<EmployeDto> employeDtoList) {
+//        return employeDtoList.toString();
+//    }
+
 }
